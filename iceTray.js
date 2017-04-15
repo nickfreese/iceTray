3e91a9947a7236a1ccf7831a985c14f07818431a
Nick Freese <nickfreese@Nicks-MacBook-Pro.local>
2017-04-15 17:29:28 -0400

    first commit, no readme yet

---
+++ iceTray.js
@@ -0,0 +1,178 @@
+/*-------------------------------
+
+Ice Tray - Copyright Nick Freese 2017
+
+-------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+
+Why use iceTray?  Ice Tray lets you progressively load your content and assets.  You can prioritize your most important content to manage the UX in the first crucial moments as your site loads.
+
+Description:  iceTry is a js library for asyncronously loading content.
+
+How it works:
+
+you create a new iceTray object and define a 'requests' object.  This object takes objects numbered started from zero.   
+
+example: requests: {0:{}, 1:{}, 2:{}};
+
+each number object requires a 'url' parameter specifying a source.
+
+All other parameters are optional.
+
+dependencies: [2, 4], 
+
+responseType: 'response type', //defaults to ''
+
+method: 'POST or GET' //defaults to GET
+
+placement:  domNode //in the case of loading external JS or CSS this is the dom node which you would like to append the script
+
+response: //stores the request response.  is not parameter;
+
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------*/
+
+var iceTray = function() {
+    
+    this.complete = false;
+
+    this.failed = []; //log failed requesy keys here
+
+    this.isImage = function checkURL(url) {
+        return(url.match(/\.(jpeg|jpg|gif|png|webp|svg)$/) != null);
+    };
+    this.isScript = function checkURL(url) {
+        return(url.match(/\.(js)$/) != null);
+    };
+    this.isCss = function checkURL(url) {
+        return(url.match(/\.(css)$/) != null);
+    };
+    
+    this.chain = function(key) {
+
+        //check dependancies
+        var skipRequest = false;
+        if (this.failed !== []) {
+            for (i = 0; i < this.failed.length; i++) {
+
+                if (this.requests[key].dependencies !== undefined && this.requests[key].dependencies.includes(this.failed[i])) {
+                    skipRequest = true;
+                }
+
+            }
+        }
+        //load ajax content
+        if (skipRequest == false && this.requests[key].placement == undefined && !this.isImage(this.requests[key].url) && !this.isScript(this.requests[key].url) && !this.isCss(this.requests[key].url)) {
+            var request = this.requests[key];
+            var _this = this;
+            //set method option  
+            if (request.method == undefined) {
+                request.method = 'GET';
+            }
+            //set datatype
+            if (request.responseType == undefined) {
+                request.responseType = "";
+            }
+
+            var promise = new Promise(function(resolve, reject) {
+
+                var iceAjax = new XMLHttpRequest();
+                iceAjax.responseType = request.responseType;
+                iceAjax.open('GET', request.url);
+                iceAjax.onload = function() {
+                    if (iceAjax.status == 200) {
+                        resolve(iceAjax.response); // we got data here, so resolve the Promise
+                    } else {
+                        reject(Error(iceAjax.statusText)); // status is not 200 OK, so reject
+                    }
+                }
+                iceAjax.onerror = function() {
+                    reject(Error('Error fetching data.')); // error occurred, reject the  Promise
+                };
+
+                iceAjax.send(); //send the request
+
+            });
+
+            promise.then(function(data) {
+                _this.requests[key].response = data;
+                if (_this.requests[key].callback !== undefined) {
+                    _this.requests[key].callback(data);
+                }
+
+                key = key + 1;
+                if (_this.requests[key] !== undefined) {
+                    _this.chain(key);
+                }
+            }, function(error) {
+
+                _this.failed.push(key);
+                key = key + 1;
+                if (_this.requests[key] !== undefined) {
+                    _this.chain(key);
+                }
+
+            });
+        }
+        //load image
+        else if(skipRequest == false && this.isImage(this.requests[key].url)){
+            
+            var _this = this;
+            var img = document.createElement('img');
+            img.onload = function(){
+                _this.requests[key].response = img;
+                _this.requests[key].callback();
+                key = key + 1;
+                if (_this.requests[key] !== undefined) {
+                    _this.chain(key);
+                }
+            }
+            img.src = this.requests[key].url;
+            
+        }
+        //load script
+        else if (skipRequest == false && this.isScript(this.requests[key].url) && this.requests[key].placement !== undefined) {
+            var _this = this;
+            var script = document.createElement('script');
+            script.onload = function() {
+                _this.requests[key].callback();
+                key = key + 1;
+                if (_this.requests[key] !== undefined) {
+                    _this.chain(key);
+                }
+            };
+            script.src = this.requests[key].url;
+            _this.requests[key].placement.appendChild(script);
+        }
+        //load css
+        else if (skipRequest == false && this.isCss(this.requests[key].url) && this.requests[key].placement !== undefined) {
+            var _this = this;
+            var css = document.createElement('link');
+
+            css.onload = function() {
+                _this.requests[key].callback();
+                key = key + 1;
+                if (_this.requests[key] !== undefined) {
+                    _this.chain(key);
+                }
+
+            };
+            css.type = "text/css";
+            css.rel = "stylesheet";
+            css.href = this.requests[key].url;
+            _this.requests[key].placement.appendChild(css);
+        }
+        //skip and push to failed
+        else {
+            this.failed.push(key);
+            key = key + 1;
+            if (this.requests[key] !== undefined) {
+                this.chain(key);
+            }
+        }
+    };
+};
